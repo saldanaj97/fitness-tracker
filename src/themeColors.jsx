b67d34f087db6093886393100f511e3d2465b102
Juan Saldana <saldanaj97@gmail.com>
2023-12-20 14:52:41 -0600

    theme

---
+++ src/themeColors.jsx
@@ -0,0 +1,20 @@
+export const darkModeColors = {
+  primary: '#050953',
+  secondary: '#E3E5EE',
+  primaryAccent: '#FFCBE1',
+  secondaryAccent: '#0fCEAE',
+  primaryFont: '#E3E5EE',
+  secondaryFont: '#3E3E46',
+  detailFont: '#B7B5C8',
+}
+
+export const lightModeColors = {
+  primary: '#E3E5EE',
+  secondary: '#050953',
+  primaryAccent: '#050953',
+  secondaryAccent: '#0fCEAE',
+  thirdAccent: '#FFCBE1',
+  primaryFont: '#050953',
+  secondaryFont: '#E3E5EE',
+  detailFont: '#B7B5C8',
+}
