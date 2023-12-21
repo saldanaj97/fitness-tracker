df6e7868699265cae848eaba5a07877bb04c513d
Juan Saldana <saldanaj97@gmail.com>
2023-12-20 19:36:07 -0600

    theme setuop

--- App.tsx
+++ App.tsx
@@ -6,27 +6,57 @@
  */
 
 import React from 'react'
-import { SafeAreaView, ScrollView, StatusBar, useColorScheme, View } from 'react-native'
+import { SafeAreaView, ScrollView, StatusBar, StyleSheet, useColorScheme, View } from 'react-native'
 
 import Activity from './src/components/activity/activity'
-import useThemeStyles from './src/hooks/theme'
+import { DarkModeColors, LightModeColors } from './src/themeColors'
 
 function App(): React.JSX.Element {
   const isDarkMode = useColorScheme() === 'dark'
-  const { backgroundStyle, viewStyle } = useThemeStyles(isDarkMode)
+  const styles = isDarkMode ? darkModeStyles : lightModeStyles
 
   return (
-    <SafeAreaView style={backgroundStyle}>
+    <SafeAreaView style={styles.container}>
       <StatusBar
         barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+        backgroundColor={isDarkMode ? DarkModeColors.primary : LightModeColors.primary}
       />
-      <ScrollView contentInsetAdjustmentBehavior="automatic" style={backgroundStyle}>
-        <View style={viewStyle} />
-        <Activity />
+      <ScrollView contentInsetAdjustmentBehavior="automatic">
+        <View style={styles.view} />
       </ScrollView>
+      <Activity />
     </SafeAreaView>
   )
 }
+
+const darkModeStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: DarkModeColors.primary,
+  },
+  text: {
+    color: DarkModeColors.primaryFont,
+  },
+  view: {
+    backgroundColor: DarkModeColors.primary,
+    margin: 10,
+  },
+})
+
+const lightModeStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center',
+    backgroundColor: LightModeColors.primary,
+  },
+  text: {
+    color: LightModeColors.primaryFont,
+  },
+  view: {
+    backgroundColor: LightModeColors.primary,
+    margin: 10,
+  },
+})
 
 export default App
